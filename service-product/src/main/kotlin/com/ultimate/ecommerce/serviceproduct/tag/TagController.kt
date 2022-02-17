2ec8a03f558dcc178c1922a4995ab6d4a8e61a15
obabichev <babichev.oleg.n@gmail.com>
2022-02-17 13:25:16 +0100

    tags

---
+++ service-product/src/main/kotlin/com/ultimate/ecommerce/serviceproduct/tag/TagController.kt
@@ -0,0 +1,14 @@
+package com.ultimate.ecommerce.serviceproduct.tag
+
+import com.ultimate.ecommerce.serviceproduct.tag.model.MOCK_TAGS
+import com.ultimate.ecommerce.serviceproduct.tag.model.Tag
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TagController {
+    @GetMapping("/api/tag")
+    fun getTags(): List<Tag> {
+        return MOCK_TAGS
+    }
+}
---
+++ service-product/src/main/kotlin/com/ultimate/ecommerce/serviceproduct/tag/model/Tag.kt
@@ -0,0 +1,24 @@
+package com.ultimate.ecommerce.serviceproduct.tag.model
+
+data class Tag(
+    val key: String,
+    val parent: String?,
+    val title: String
+)
+
+val MOCK_TAGS = listOf(
+    Tag("books", null, "Books"),
+    Tag("computing-and-internet", "books", "Computing & Internet"),
+    Tag("computing-and-internet-databases", "computing-and-internet", "Computing & Internet Databases"),
+    Tag("it-training-and-professions", "computing-and-internet", "IT Training & Professions"),
+    Tag("graphics-and-multimedia-software", "computing-and-internet", "Graphics & Multimedia Software"),
+    Tag("hardware", "computing-and-internet", "Hardware"),
+    Tag("video-game-strategy-guides", "computing-and-internet", "Video Game Strategy Guides"),
+    Tag("mac-os-applications", "computing-and-internet", "Mac OS Applications"),
+    Tag("unix-and-linux", "computing-and-internet", "Unix & Linux"),
+    Tag("business-and-careers", "books", "Business & Careers"),
+    Tag("business-finance-and-law-cvs", "business-and-careers", "Business, Finance & Law CVs"),
+    Tag("accounting", "business-and-careers", "Accounting"),
+    Tag("e-business", "business-and-careers", "E-Business"),
+    Tag("professional-finance", "business-and-careers", "Professional Finance")
+)
